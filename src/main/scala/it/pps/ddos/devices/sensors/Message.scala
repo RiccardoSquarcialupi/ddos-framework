b334b74133c337de0ead62c9127c0de65e868805
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2022-11-18 18:01:44 +0100

    redefine sensor type messages

--- src/main/scala/it/pps/ddos/devices/sensors/Message.scala
+++ src/main/scala/it/pps/ddos/devices/sensors/Message.scala
@@ -2,6 +2,5 @@ package it.pps.ddos.devices.sensors
 
 trait Message
 
-case class SetStatus[T](status: T) extends Message
-case class GetStatus() extends Message
+case class SendStatus(master: ActorRef) extends Message
 case class SelfMessage() extends Message

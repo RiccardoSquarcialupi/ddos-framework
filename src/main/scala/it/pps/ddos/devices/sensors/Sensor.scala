cbed393220ab4fe3c2269d867acb76205fe26aff
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-11-21 19:22:27 +0100

    PPS-15 #comment minor fix Sensor,BasicSensor,ProcessedDataSensor

--- src/main/scala/it/pps/ddos/devices/sensors/Sensor.scala
+++ src/main/scala/it/pps/ddos/devices/sensors/Sensor.scala
@@ -1,25 +1,30 @@
 package it.pps.ddos.devices.sensors
 
+import akka.actor.typed.ActorRef
+import it.pps.ddos.devices.sensors.SensorProtocol.{Message, Status}
+
 import scala.concurrent.duration.FiniteDuration
 
 /*
 * Define logic sensors
 * */
-trait Sensor[A, B](val destination: ActorRef[_]):
+trait Sensor[A, B](val destination: ActorRef[Status[_]]):
   var status: Option[A] = Option.empty
 
   def preProcess: B => A
-  def update(physicalInput: B) = status = Option(preProcess(physicalInput))
+
+  def update(physicalInput: B): Unit = status = Option(preProcess(physicalInput))
+
   def propagate(sensorID: ActorRef[Message], requester: ActorRef[Message]): Unit = status match
     case Some(value) => destination ! Status[A](sensorID, value)
-    case None() =>
+    case None =>
 
 
 class BasicSensor[A](destination: ActorRef[Status[_]]) extends Sensor[A, A](destination) :
-  override def processingFunction: A => A = x => x
+  override def preProcess: A => A = x => x
 
 class ProcessedDataSensor[A, B](destination: ActorRef[Status[_]], processFun: B => A) extends Sensor[A, B](destination) :
-  override def processingFunction: B => A = processFun
+  override def preProcess: B => A = processFun
 
 /*
 * Actor of a basic sensor

b70eba50fa69083d9f0a500b8319e992ab8e9b0d
RiccardoSquarcialupi <rikysquarcia@gmail.com>
2022-11-21 18:42:46 +0100

    PPS-12 #comment added a private message for the TimedSensorActor

--- src/main/scala/it/pps/ddos/devices/sensors/TimedSensorActor.scala
+++ src/main/scala/it/pps/ddos/devices/sensors/TimedSensorActor.scala
@@ -2,20 +2,25 @@ package it.pps.ddos.devices.sensors
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import it.pps.ddos.devices.sensors.SelfMessage
+import it.pps.ddos.devices.actuators.Message
 import it.pps.ddos.devices.sensors.module.TimedModule
 
 private case object SensorTimerKey
 
 object TimedSensorActor:
 
+    /**
+     * declaration of the the private message for the timed actor
+     */
+    private case object Tick extends Message
+
     def apply[A](timedSensor: TimedModule[A]): Behavior[Message] =
         Behaviors.setup { _ =>
             Behaviors.withTimers { timer =>
-                timer.startTimerAtFixedRate(SensorTimerKey, SelfMessage(), timedSensor.duration)
+                timer.startTimerAtFixedRate(SensorTimerKey, Tick, timedSensor.duration)
                 Behaviors.receiveMessage { message =>
                     message match
-                        case SelfMessage() =>
+                        case Tick =>
                             timedSensor.setStatus(timedSensor.status)
                             Behaviors.same
                 }

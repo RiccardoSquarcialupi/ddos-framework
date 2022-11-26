78c0377665d267ca735e708af6b7db53fe112778
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2022-11-26 22:41:40 +0100

    PPS-8 #comment add minor refactor of sensor protocol

--- src/main/scala/it/pps/ddos/device/sensor/SensorProtocol.scala
+++ src/main/scala/it/pps/ddos/device/sensor/SensorProtocol.scala
@@ -5,16 +5,16 @@ import akka.actor.typed.ActorRef
 /* Definition of the message protocol shared by the sensors */
 object SensorProtocol:
   trait Message
-  case class PropagateStatus(requester: ActorRef[Message]) extends Message
+  final case class PropagateStatus(requester: ActorRef[Message]) extends Message
 
-  case class Status[T](author: ActorRef[Message], value: T) extends Message
+  final case class Status[T](author: ActorRef[Message], value: T) extends Message
 
-  case class UpdateStatus[T](value: T) extends Message
+  final case class UpdateStatus[T](value: T) extends Message
 
-  case class Subscribe(replyTo: ActorRef[Message]) extends Message
+  final case class Subscribe(replyTo: ActorRef[Message]) extends Message
 
-  case class SubscribeAck(author: ActorRef[Message]) extends Message
+  final case class SubscribeAck(author: ActorRef[Message]) extends Message
 
-  case class Unsubscribe(replyTo: ActorRef[Message]) extends Message
+  final case class Unsubscribe(replyTo: ActorRef[Message]) extends Message
 
-  case class UnsubscribeAck(author: ActorRef[Message]) extends Message
+  final case class UnsubscribeAck(author: ActorRef[Message]) extends Message

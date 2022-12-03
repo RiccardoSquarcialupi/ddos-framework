341033cfa9f06c49b329ba2293fe9885ac4378a8
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-12-03 20:21:43 +0100

    PPS-26 #comment group actor second draft

--- src/main/scala/it/pps/ddos/grouping/Group.scala
+++ src/main/scala/it/pps/ddos/grouping/Group.scala
@@ -11,39 +11,63 @@ import scala.concurrent.duration.FiniteDuration
 
 trait GroupActor:
   // initial state
-  def apply(group: Group[_]): Behavior[Message] =
+  def apply[I,O](group: Group[I,O]): Behavior[Message] =
     Behaviors.setup {
       context =>
         group.sources.foreach(_ ! Subscribe(context.self))
-        connecting(group.sources, group, context)
+        connecting(group.sources, group)
     }
 
-  private def connecting(sources: List[ActorRef[Message]], group: Group[_], context: ActorContext[Message]): Behavior[Message] =
+  def connecting[I,O](sources: List[ActorRef[Message]], group: Group[I,O]): Behavior[Message] =
     Behaviors.withTimers[Message] { timer =>
       timer.startTimerAtFixedRate(Timeout(), FiniteDuration(5, "second"))
-      Behaviors.receiveMessagePartial { message =>
+      Behaviors.receivePartial { (context, message) =>
         (message, sources) match
           case (Timeout(), _) =>
             sources.foreach(_ ! Subscribe(context.self))
-            connecting(sources, group, context)
-          case (SubscribeAck(author), destinations) if destinations.length > 1 => connecting(destinations.filter(_ != author), group, context)
-          case (SubscribeAck(author), destinations) if destinations.exists(_ == author) => active(group.sources, group, context)
-          case _ => connecting(sources, group, context)
+            connecting(sources, group)
+          case (SubscribeAck(author), sources) if sources.length > 1 =>
+            connecting(sources.filter(_ != author), group)
+          case (SubscribeAck(author), sources) if sources.contains(author) =>
+            active(group.sources, group)
+          case _ =>
+            connecting(sources, group)
       }
     }
 
-  def active(sources: List[ActorRef[Message]], group: Group[_], context: ActorContext[Message]): Behavior[Message]
+  def active[I,O](sources: List[ActorRef[Message]], group: Group[I,O]): Behavior[Message]
 
 object BlockingGroup extends GroupActor:
-  override def active(group: Group[_], context: ActorContext[Message]): Behavior[Message] =
-    Behaviors.receive[Message] { (message, context) =>
+  private case class CrossOut(source: ActorRef[Message]) extends Message
+
+  override def active[I,O](sources: List[ActorRef[Message]], group: Group[I,O]): Behavior[Message] =
+    Behaviors.receive{ (context, message) =>
       message match
-        case Status(author, value) =>
-        case Statuses(author, values) =>
-        case _ =>
+        case Status[I](author, value) =>
+          context.self ! Status[List[I]](author, List(value))
+          Behaviors.same
+        case Status[List[I]](author, value) if group.sources.contains(author) =>
+          group.insert(author, value)
+          context.self ! CrossOut(author)
+          Behaviors.same
+        case CrossOut(source) if sources.length > 1 =>
+          active(sources.filter(_ != source), group)
+        case CrossOut(source) if sources.contains(source) =>
+          group.compute
+          active(group.sources, group)
+        case _ => Behaviors.same
     }
 
-class Group[T](val sources: List[ActorRef[Message]]) {
-  var values: Map[ActorRef[Message], T]
+trait Group[I,O](val sources: List[ActorRef[Message]]):
+  var values: Map[ActorRef[Message], List[I]]
+  def insert(author: ActorRef[Message], newValues: List[I]): Unit = values = values + (author -> newValues)
+  def compute(signature: ActorRef[Message]): Unit
 
-}
+class MapGroup[I,O](val sources: List[ActorRef[Message]], val f: I => O, val dest:ActorRef[Message]) extends Group[I,O](sources):
+  override def compute(signature: ActorRef[Message]): Unit =
+    var listResult: List[O]
+    for (i <- values.values.flatten) yield listResult = listResult + f(i)
+    dest ! Status[List[O]](signature, listResult)
+
+
+

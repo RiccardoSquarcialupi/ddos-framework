aa2331b8c87e810f97457f7a10182549ee3cc4e4
Filippo Cavallari <filippo.cavalallari99@gmail.com>
2022-12-06 19:21:46 +0100

    PPS-21 Add test for deployer method. Fix cluster setup

--- src/main/scala/it/pps/ddos/deployment/Deployer.scala
+++ src/main/scala/it/pps/ddos/deployment/Deployer.scala
@@ -17,44 +17,61 @@ object Deployer:
 
   private val orderedActorSystemRefList = mutable.ListBuffer.empty[ActorSysWithActor]
 
-  private val cluster = Cluster(system = ActorSystem(Behaviors.same[Message], "ClusterSystem", setupClusterConfig("2551")))
+  private var cluster: Option[Cluster] = None
+
   private var devicesActorRefMap = Map.empty[String, ActorRef[Message]]
 
 
   def init(numberOfNode: Int): Unit =
     for (i <- 1 to numberOfNode) {
-      val actorSystem = ActorSystem(Behaviors.setup(
-        context =>
-          Behaviors.receiveMessage { msg =>
-            msg match
-              case InternSpawn(id, behavior) =>
-                devicesActorRefMap = Map((id, context.spawn(behavior, "Actuator"))) ++ devicesActorRefMap
-                Behaviors.same
-          }
-      ), "ActorSystem-" + i)
-      Cluster(system = actorSystem).manager ! Join(cluster.selfMember.address)
+      val actorSystem = if (cluster.isEmpty)
+        val as = ActorSystem(Behaviors.empty, "ClusterSystem", setupClusterConfig("2551"))
+        cluster = Some(Cluster(as))
+        as
+      else
+        val as = createActorSystem("ActorSystem-" + i)
+        Cluster(system = as).manager ! Join(cluster.get.selfMember.address)
+        as
       orderedActorSystemRefList += ActorSysWithActor(actorSystem, 0)
     }
 
-  private def deploy(devices: Actuator[Message]*): Unit =
+
+  private def createActorSystem(id: String): ActorSystem[InternSpawn] =
+    ActorSystem(Behaviors.setup(
+      context =>
+        Behaviors.receiveMessage { msg =>
+          msg match
+            case InternSpawn(id, behavior) =>
+              devicesActorRefMap = Map((id, context.spawn(behavior, "Actuator"))) ++ devicesActorRefMap
+              Behaviors.same
+        }
+    ), id)
+
+  private def deploy[T](devices: Actuator[T]*): Unit =
     devices.foreach(dev =>
       val actorRefWithInt = orderedActorSystemRefList.filter(_.actorSystem.ref == getMinSpawnActorNode).head
       actorRefWithInt.actorSystem.ref ! InternSpawn(dev.id, dev.getBehavior)
       actorRefWithInt.numberOfActorSpawned + 1
     )
 
-  def deploy(devicesGraph: Graph[Actuator[Message]]): Unit =
+  def deploy[T](devicesGraph: Graph[Actuator[T]]): Unit =
     devicesGraph @-> ((k,_) => deploy(k))
     devicesGraph @-> ((k, v) => v.map(it => devicesActorRefMap.get(it.id)).filter(_.isDefined).foreach(device => devicesActorRefMap(k.id).ref ! Subscribe(device.get.ref)))
 
   private def setupClusterConfig(port: String): Config =
     val hostname = "127.0.0.1"
+    println(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostname)
+      + String.format("akka.remote.artery.canonical.port=" + port + "%n")
+      + String.format("akka.management.http.hostname=\"%s\"%n",hostname)
+      + String.format("akka.management.http.port=" + port.replace("255", "855") + "%n")
+      + String.format("akka.management.http.route-providers-read-only=%s%n", "false")
+      + String.format("akka.remote.artery.advanced.tcp.outbound-client-hostname=%s%n", hostname))
     ConfigFactory.parseString(String.format("akka.remote.artery.canonical.hostname = \"%s\"%n", hostname)
-      + String.format("akka.remote.artery.canonical.port=" + port)
-      + String.format("akka.management.http.hostname =" + hostname)
-      + String.format("akka.management.http.port=" + port.replace("255", "855"))
-      + String.format("akka.management.http.route-providers-read-only = %s%n", "false")
-      + String.format("akka.remote.artery.advanced.tcp.outbound-client-hostname = %s%n", hostname))
+      + String.format("akka.remote.artery.canonical.port=" + port + "%n")
+      + String.format("akka.management.http.hostname=\"%s\"%n",hostname)
+      + String.format("akka.management.http.port=" + port.replace("255", "855") + "%n")
+      + String.format("akka.management.http.route-providers-read-only=%s%n", "false")
+      + String.format("akka.remote.artery.advanced.tcp.outbound-client-hostname=%s%n", hostname))
       .withFallback(ConfigFactory.load())
 
   private def getMinSpawnActorNode: ActorRef[InternSpawn] =

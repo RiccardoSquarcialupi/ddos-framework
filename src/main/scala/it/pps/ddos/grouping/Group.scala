29cbef635ac4edabc7fd433d6aca436719e1513a
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-12-10 17:45:59 +0100

    add MapGroup first draft for testing purposes

--- src/main/scala/it/pps/ddos/grouping/Group.scala
+++ src/main/scala/it/pps/ddos/grouping/Group.scala
@@ -12,3 +12,10 @@ abstract class Group[I,O](val sources: List[ActorRef[Message]], destinations: Li
   def insert(author: ActorRef[Message], newValues: List[I]): Unit = data = data + (author -> newValues)
   def reset(): Unit = data = Map.empty;
   def compute(signature: ActorRef[Message]): Unit
+
+class MapGroup[I,O](sources: List[ActorRef[Message]],
+                    destinations: List[ActorRef[Message]],
+                    val f: I => O) extends Group[I, List[O]](sources, destinations):
+  override def compute(signature: ActorRef[Message]): Unit =
+    status = Option.empty
+    for (i <- data.values.flatten) yield status = Option(status.getOrElse(List.empty) ++ List(f(i)))

b89c3fc92212ee2d5317952b9cf97a95a9f3405f
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2022-12-22 22:30:47 +0100

    PPS-23 #comment minor refactor in ReduceGroup (use foldLeft instead of reduce)

--- src/test/scala/it/pps/ddos/group/GroupTest.scala
+++ src/test/scala/it/pps/ddos/group/GroupTest.scala
@@ -22,6 +22,7 @@ class GroupTest extends AnyFlatSpec:
   it should "be possible to update already stored values until computation is triggered" in testUpdatingValues()
   "A non-blocking group of devices" should "trigger its computation whenever a new value is received from the sources list" in testNonBlockingBehavior()
   "A ReduceGroup" should "reduce the whole list of values in an aggregated single value" in testReduce()
+  it should " " in test()
 
   val testKit: ActorTestKit = ActorTestKit()
 
@@ -119,8 +120,14 @@ class GroupTest extends AnyFlatSpec:
 
   private def testReduce(): Unit =
     resetVariables()
-    val toUppercaseActor = testKit.spawn(BlockingGroup(new ReduceGroup[String]("id", sensors, List(testProbe.ref), _ + " | " + _)))
+    val toUppercaseActor = testKit.spawn(BlockingGroup(new ReduceGroup[String, String]("id", sensors, List(testProbe.ref), _ + " | " + _, "")))
     for s <- sensors yield s ! UpdateStatus("status of sensor")
     Thread.sleep(500)
     for s <- sensors yield s ! PropagateStatus(testProbe.ref); Thread.sleep(500)
-    testProbe.expectMessage(Status(toUppercaseActor, "status of sensor | status of sensor | status of sensor"))
+    testProbe.expectMessage(Status(toUppercaseActor, " | status of sensor | status of sensor | status of sensor"))
+
+  private def test(): Unit =
+    val f = (a: String, b: Int) => a + " " + b.toString()
+    val l = List(1, 2, 3)
+    val res = l.foldLeft("")(f)
+    assert(res == " 1 2 3")

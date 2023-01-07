39ae34277f47478746fdb35727e71b13dc660e68
BrunoEsposito2 <bruno.esposito2@studio.unibo.it>
2023-01-07 10:42:31 +0100

    PPS-32 #comment add TusowBinder actor for sensor data storage

--- src/main/scala/it/pps/ddos/storage/tusow/TusowBinder.scala
+++ src/main/scala/it/pps/ddos/storage/tusow/TusowBinder.scala
@@ -8,7 +8,7 @@ import it.pps.ddos.deployment.graph.Graph
 import it.pps.ddos.device.{Device, Public, Timer}
 import it.pps.ddos.device.DeviceProtocol.{Message, PropagateStatus, Status, Subscribe}
 import it.pps.ddos.device.sensor.BasicSensor
-import it.pps.ddos.storage.tusow.TusowBinder.TUSOW_SYSTEM_NAME
+import it.pps.ddos.storage.tusow.TusowBinder.{ACTOR_SYSTEM_NAME, TUSOW_SYSTEM_NAME, getConfigFile}
 import it.pps.ddos.storage.tusow.client.Client
 import it.unibo.coordination.tusow.grpc.{IOResponse, ReadOrTakeRequest, Template, Tuple, TupleSpaceID, TupleSpaceType, TusowServiceClient, WriteRequest}
 import it.pps.ddos.utils.GivenDataType.given
@@ -22,41 +22,17 @@ import scala.concurrent.duration.Duration
 
 object TusowBinder:
   private final val TUSOW_SYSTEM_NAME = "ddos-tusow-storage"
+  private final val ACTOR_SYSTEM_NAME = "ClusterSystem"
 
-  @main
-  def main(): Unit =
+  private def setupDeployer(numNodes: Int): Unit =
+    Deployer.initSeedNodes()
+    Deployer.addNodes(numNodes)
+    Server.start()
 
-    setup()
-
-    implicit val sys: ActorSystem[Message] = ActorSystem(Behaviors.empty, "ClusterSystem", getConfigFile)
-    implicit val ec: ExecutionContextExecutor = sys.classicSystem.dispatcher
-
-    val tupleSpace = new TupleSpaceID(TUSOW_SYSTEM_NAME, TupleSpaceType.LOGIC)
-    val listOfRef = ListBuffer[ActorRef[Message]]()
-
-    Deployer.deploy(createBasicStateSensors(sys))
-    Thread.sleep(5000)
-    for (ref <- Deployer.getDevicesActorRefMap.toList)
-      listOfRef.addOne(ref._2)
-
-    val client = Client.createClient(sys.classicSystem, ec)
-
-    client.createTupleSpace(tupleSpace).onComplete(response =>
-      sys.systemActorOf(Behaviors.setup({
-        context =>
-          for (actor <- listOfRef.toList)
-            actor ! Subscribe(context.self)
-
-          Behaviors.receiveMessage({
-            case Status(ref, value) =>
-              val tuple = new Tuple(TUSOW_SYSTEM_NAME, "data(" + "'" + ref.toString + "'," + value.toString + ").")
-              val writeResponse = Await.result[IOResponse](client.write(new WriteRequest(Some(tupleSpace), Some(tuple))), Duration(5000, TimeUnit.MILLISECONDS))
-              println(writeResponse)
-              Behaviors.same
-            case _ => Behaviors.same
-          })
-      }), "TusowActor")
-    )
+  private def getConfigFile: Config =
+    val configFile = getClass.getClassLoader.getResource("application.conf").getFile
+    val config = ConfigFactory.parseFile(new File(configFile))
+    config
 
   private def createBasicStateSensors(actorSys: ActorSystem[Message]): Graph[Device[Double]] =
     val basic1 = new BasicSensor[Double]("0", List.empty) with Public[Double] with Timer(Duration(1, TimeUnit.SECONDS))
@@ -71,15 +47,37 @@ object TusowBinder:
     )
     graph
 
-  private def getConfigFile: Config =
-    val configFile = getClass.getClassLoader.getResource("application.conf").getFile
-    val config = ConfigFactory.parseFile(new File(configFile))
-    config
+  def apply(sys: ActorSystem[Message] = ActorSystem(Behaviors.empty, ACTOR_SYSTEM_NAME, getConfigFile)) =
+    implicit val ec: ExecutionContextExecutor = sys.classicSystem.dispatcher
 
-  private def setup(): Unit =
-    Deployer.initSeedNodes()
-    Deployer.addNodes(5)
-    Server.start()
+    val tupleSpace = new TupleSpaceID(TUSOW_SYSTEM_NAME, TupleSpaceType.LOGIC)
+
+    setupDeployer(5)
+    Deployer.deploy(createBasicStateSensors(sys))
+    Thread.sleep(5000)
+
+    val actorList = Deployer.getDevicesActorRefMap.values.toList
+    val client = Client.createClient(sys.classicSystem, ec)
+
+    client.createTupleSpace(tupleSpace).onComplete(response =>
+      sys.systemActorOf(Behaviors.setup({
+        context =>
+          actorList.foreach(actor => actor ! Subscribe(context.self))
+
+          Behaviors.receiveMessage({
+            case Status(ref, value) =>
+              val tuple = new Tuple(TUSOW_SYSTEM_NAME, "data(" + "'" + ref.toString + "'," + value.toString + ").")
+              val writeResponse = Await.result[IOResponse](client.write(new WriteRequest(Some(tupleSpace), Some(tuple))), Duration(5000, TimeUnit.MILLISECONDS))
+              println(writeResponse)
+              Behaviors.same
+            case _ => Behaviors.same
+          })
+      }), "TusowActor")
+    )
+
+/* Runnable TusowBinder actor for debug */
+//object TusowBinderMain extends App:
+  //TusowBinder()
 
 
 

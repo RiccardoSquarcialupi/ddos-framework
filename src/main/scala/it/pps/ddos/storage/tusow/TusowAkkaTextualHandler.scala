38e62d64c6e35d2bd3d5bb3d10b3c3238f4dd10f
Filippo Cavallari <filippo.cavalallari99@gmail.com>
2023-01-08 21:19:43 +0100

    PPS-31 #comment Refactor code to remove duplicates

--- src/main/scala/it/pps/ddos/storage/tusow/TusowAkkaTextualHandler.scala
+++ src/main/scala/it/pps/ddos/storage/tusow/TusowAkkaTextualHandler.scala
@@ -27,19 +27,9 @@ class TusowAkkaTextualHandler extends TusowService:
         textualSpaces(in.id) = TextualSpace.local(in.id)
         Future.successful(IOResponse(response = true))
 
-    private def handleFutureRequest[A](space: TextualSpace)(failureHandler: () => Future[A])(successHandler: () => Future[A]): Future[A] =
-        if (space == null)
-            failureHandler()
-        else
-            successHandler()
-
     override def write(in: WriteRequest): Future[IOResponse] =
         val space = textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id)
         handleFutureRequest(space)(() => Future.successful(IOResponse(response = false, message = "Tuple space not found")))(() => space.write(in.tuple.get.value).toScala.map(f => IOResponse(response = true, message = f.toString)))
-
-    private def handleReadOrTakeRequest[A](in: ReadOrTakeRequest)(readOrTake: (TextualSpace, String, Long) => Future[A]): Future[A] =
-        val space = textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id)
-        handleFutureRequest(space)(() => Future.failed(new IllegalArgumentException("Tuple space not found")))(() => readOrTake(space, in.template.textualTemplate.get.regex, timeout))
 
     override def read(in: ReadOrTakeRequest): Future[Tuple] =
        handleReadOrTakeRequest(in)((space, template, timeout) => space.read(RegexTemplate.of(template)).orTimeout(timeout, TimeUnit.SECONDS).toScala.map(t => Tuple(t.getTemplate.toString, t.getTuple.orElse(StringTuple.of("")).getValue)))
@@ -55,21 +45,11 @@ class TusowAkkaTextualHandler extends TusowService:
             TusowGRPCCommons.processWriteAllFutures(futures)(f => f.getValue)
         )
 
-    private def handleReadOrTakeAllRequest[A](in: ReadOrTakeAllRequest)(readOrTake: (TextualSpace, Seq[String], Long) => Future[A]): Future[A] =
-        val space = textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id)
-        handleFutureRequest(space)(() => Future.failed(new IllegalArgumentException("Tuple space does not exist")))(() =>
-            readOrTake(space, in.templates.textualTemplateList.get.regexes.map(regex => regex.regex), timeout)
-        )
-
-    private def processReadOrTakeAllFutures(futures: Seq[CompletableFuture[RegularMatch]]): Future[TuplesList] =
-        TusowGRPCCommons.processReadOrTakeAllFutures(futures)(f => Tuple(f.getTemplate.toString, f.getTuple.get().getValue))
-
     override def readAll(in: ReadOrTakeAllRequest): Future[TuplesList] =
         handleReadOrTakeAllRequest(in)((space, templates, timeout) =>
             val futures = templates.map(space.read(_).orTimeout(timeout, TimeUnit.SECONDS))
             processReadOrTakeAllFutures(futures)
         )
-
 
     override def takeAll(in: ReadOrTakeAllRequest): Future[TuplesList] =
         handleReadOrTakeAllRequest(in)((space, templates, timeout) =>
@@ -85,10 +65,6 @@ class TusowAkkaTextualHandler extends TusowService:
                 TusowGRPCCommons.joinFutures(futures)
                 Source(futures.map(f => IOResponse(response = true, message = f.get.toString)).toList)
 
-    private def processStreamFutures(futures: Seq[CompletableFuture[RegularMatch]]): Source[Tuple, NotUsed] =
-        TusowGRPCCommons.joinFutures(futures)
-        Source(futures.map(f => Tuple(f.get().getTemplate.toString, f.get().getTuple.get().getValue)).toList)
-
     override def readAllAsStream(in: ReadOrTakeAllRequest): Source[Tuple, NotUsed] =
         textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id) match
             case null => Source.empty
@@ -101,3 +77,26 @@ class TusowAkkaTextualHandler extends TusowService:
             case null => Source.empty
             case space@_ =>
                 processStreamFutures(in.templates.textualTemplateList.get.regexes.map(t => space.take(t.regex)))
+
+    private def processStreamFutures(futures: Seq[CompletableFuture[RegularMatch]]): Source[Tuple, NotUsed] =
+        TusowGRPCCommons.joinFutures(futures)
+        Source(futures.map(f => Tuple(f.get().getTemplate.toString, f.get().getTuple.get().getValue)).toList)
+
+    private def handleReadOrTakeAllRequest[A](in: ReadOrTakeAllRequest)(readOrTake: (TextualSpace, Seq[String], Long) => Future[A]): Future[A] =
+        val space = textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id)
+        handleFutureRequest(space)(() => Future.failed(new IllegalArgumentException("Tuple space does not exist")))(() =>
+            readOrTake(space, in.templates.textualTemplateList.get.regexes.map(regex => regex.regex), timeout)
+        )
+
+    private def processReadOrTakeAllFutures(futures: Seq[CompletableFuture[RegularMatch]]): Future[TuplesList] =
+        TusowGRPCCommons.processReadOrTakeAllFutures(futures)(f => Tuple(f.getTemplate.toString, f.getTuple.get().getValue))
+
+    private def handleFutureRequest[A](space: TextualSpace)(failureHandler: () => Future[A])(successHandler: () => Future[A]): Future[A] =
+        if (space == null)
+            failureHandler()
+        else
+            successHandler()
+
+    private def handleReadOrTakeRequest[A](in: ReadOrTakeRequest)(readOrTake: (TextualSpace, String, Long) => Future[A]): Future[A] =
+        val space = textualSpaces(in.tupleSpaceID.getOrElse(TupleSpaceID("")).id)
+        handleFutureRequest(space)(() => Future.failed(new IllegalArgumentException("Tuple space not found")))(() => readOrTake(space, in.template.textualTemplate.get.regex, timeout))
